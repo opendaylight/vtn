41c9e63cfd0123c52d8e5b56e4b498f9ecb73fe7
gvrangan <venkatrangang@hcl.com>
2013-11-14 01:17:19 +0530

    FT Scripts for testing the VTN Coordinator Interface
      * Tests for Controller Operations
      * Tests for VTN/VBR Operations on single controller
    
    Change-Id: I20153cace6c9b025e55c67b49c13e532e5a79ea9
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

---
+++ coordinator/test/vtn_ft/controller.py
@@ -0,0 +1,397 @@
+#
+# Copyright (c) 2013 NEC Corporation
+# All rights reserved.
+# 
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this
+# distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+#
+
+
+
+#! /usr/bin/python
+
+import requests, json, collections, time
+import vtn_testconfig
+CONTROLLERDATA = vtn_testconfig.CONTROLLERDATA 
+coordinator_url=vtn_testconfig.coordinator_url
+def_header=vtn_testconfig.coordinator_headers
+
+
+
+def is_controller_deleted(controller_url=""):
+  url= coordinator_url + controller_url
+  print url
+  r = requests.get(url,headers=def_header)
+  print r.status_code
+  if r.status_code != 200:
+    return 1
+  data=json.loads(r.text)
+  print data['controller']
+  if data['controller'] != None:
+    return 1
+  else:
+    return 0
+  
+  
+def validate_controller_attributes(controller_id,ipaddr="",version="",auditstatus="",
+    description="",username="", password="",operstatus="",type="",controller_url=""):
+  url= coordinator_url + controller_url
+  print url
+  print def_header
+  r = requests.get(url,headers=def_header)
+  if r.status_code != 200:
+    return 1
+
+  data=json.loads(r.text)
+  print data
+  print data['controller']
+
+  if ipaddr != "":
+    if data['controller']['ipaddr'] != ipaddr:
+      print "IP Address does not matcch"
+      return 1
+
+  if version != "":
+   if data['controller']['version'] != version:
+     print "Version Mismatch"
+     return 1
+
+  if auditstatus != "":
+   if data['controller']['auditstatus'] != auditstatus:
+     print "Audit Status Mismatch"
+     return 1
+
+  if description != "":
+   if data['controller']['description'] != description:
+     print "Description Mismatch"
+     return 1
+
+ 
+  if username != "":
+   if data['controller']['username'] != username:
+     print "username Mismatch"
+     return 1
+
+  if password != "":
+   if data['controller']['password'] != username:
+     print "password Mismatch"
+     return 1
+
+  if operstatus != "":
+    if data['controller']['operstatus'] != operstatus:
+      print "Operstatus is not", operstatus
+      return 1
+
+  print "Validation Succeeded"
+  return 0
+
+  
+
+def add_controller(controller_id,type,version,ipaddr="",auditstatus="", description="", username = "", password = "",controller_url=""):
+  print "Add Controller"
+  print 'controller_id' ,controller_id
+  print 'ipaddrd' ,ipaddr
+  print  'type' ,type
+  print 'version' ,version
+  print 'auditstatus' ,auditstatus
+  ip_set=0
+  desc_set=0
+  uname_set=0
+  pwd_set=0
+  audistst_set=0
+
+
+  url= coordinator_url + controller_url
+  add_data = collections.defaultdict(dict)
+
+  #Add default entried
+#  add_data += "'controller_id'" + ':' + controller_id
+  add_data['controller']['controller_id']=controller_id
+#  add_data += "," + "'type'" + ':' + type
+  add_data['controller']['type']=type
+  add_data['controller']['version']=version
+  #add_data += "," + "'version'" + ':' + version
+
+
+
+  if ipaddr != "":
+    add_data['controller']['ipaddr']=ipaddr
+  else:
+    print "Not set IP Address"
+
+  if auditstatus != "":
+    add_data['controller']['auditstatus']=auditstatus
+  else:
+    print "Not set Audit Status"
+
+
+  if username != "":
+    add_data['controller']['username']=username
+  else:
+    print "No User Name"
+
+  if password != "":
+    add_data['controller']['password']=password
+  else:
+    print "No Password"
+
+  if description != "":
+    add_data['controller']['description']=description
+  else:
+   print 'Empty Description'
+    
+  print url
+  print add_data
+  print json.dumps(add_data)
+  r = requests.post(url,data=json.dumps(add_data),headers=def_header)
+  print r.status_code
+  print r.headers
+  print r.text
+  if r.status_code == 200:
+    return 0
+  else:
+    return 1
+
+
+
+def delete_controller(controller_url=""):
+  print "Delete Controller"
+  print "Delete for", controller_url 
+  url= coordinator_url + controller_url
+  print url
+  r = requests.delete(url,headers=def_header)
+  print r.status_code
+  print r.headers
+  print r.text
+  if r.status_code == 200:
+    return 0
+  else:
+    return 1
+
+
+
+def update_controller(controller_id,ipaddr="",version="",auditstatus="",
+    description="",username="", password="",controller_url=""):
+  print "Update Controller"
+  print 'controller_id', controller_id
+  
+  url= coordinator_url + controller_url
+  update_data = collections.defaultdict(dict)
+  if ipaddr != "":
+    update_data['controller']['ipaddr'] = ipaddr
+  else:
+    print "IP is not updated"
+  
+  if version != "":
+    update_data['controller']['version'] =  version
+  else:
+    print "Version not updated"
+
+
+  if auditstatus != "":
+    update_data['controller']['auditstatus'] = auditstatus 
+  else:
+    print "Audit Status not updated"
+
+
+  if description != "":
+    update_data['controller']['description'] = description
+  else:
+    print "Description is not updated"
+
+  if username != "":
+    update_data['controller']['username'] = username
+  else:
+    print "Username is not updated"
+
+  if password != "":
+    update_data['controller']['password'] = password
+  else:
+    print "Password is not updated"
+     
+  print update_data
+  print json.dumps(update_data)
+  print url
+  r = requests.put(url,data=json.dumps(update_data),headers=def_header)
+  print r.status_code
+  print r.headers
+  print r.text
+  if r.status_code == 200:
+    return 0
+  else:
+    return 1
+
+
+    
+# Tcase Begin
+
+def test_controller_ops(blockname):
+
+  print "Read from controller.data"
+  print  "Read for %s", blockname
+  test_ipaddr=vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['ipaddr']
+  test_controller_id=vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['controller_id']
+  test_description = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['description']
+  test_type = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['type']
+  test_version = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['version']
+  test_auditstatus = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['auditstatus']
+  test_username =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['username']
+  test_password =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['password']
+  test_invalusername =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['invalidusername']
+  test_invalpassword =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['invalidpassword']
+  test_url =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['url']
+  add_url=vtn_testconfig.ReadValues(CONTROLLERDATA ,'URL')['url']
+
+
+
+  print "******ADD CONTROLLER with NO IP Address**********"
+  res=add_controller(test_controller_id,test_type,test_version,controller_url=add_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  time.sleep(15)
+
+  print "******VALIDATE Attributes from Coordinator*******"
+  print "validate Mandatory Controller Attributes "
+  res = validate_controller_attributes(test_controller_id,type=test_type,version=test_version,operstatus="down",controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+
+  print "******Update IP Address************************"
+  res = update_controller(test_controller_id,ipaddr=test_ipaddr,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****VALIDATE IP ADDRESS UPDATE***************"
+  res = validate_controller_attributes(test_controller_id,type=test_type,version=test_version,operstatus="down",ipaddr=test_ipaddr,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****UPDATE AUDITSTATUS***************"
+  res = update_controller(test_controller_id,auditstatus="enable",controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  time.sleep(15)
+  print "*****CHECK AUDITSTATUS***************"
+  print "*****USES DEF USERNAME and PASSWORD***************"
+  res = validate_controller_attributes(test_controller_id,version=test_version,ipaddr=test_ipaddr,operstatus ="up",auditstatus="enable",controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****UPDATE INVALID USERNAME/PASS***************"
+  res = update_controller(test_controller_id,username=test_invalusername,password=test_invalpassword,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****CHECK OPERSTATUS DOWN***************"
+#Added to make sure audit operations complete!!
+  time.sleep (15)
+  res = validate_controller_attributes(test_controller_id,version=test_version,ipaddr=test_ipaddr,operstatus ="down",auditstatus="enable",username=test_invalusername,password=test_invalpassword,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+
+  print "*****UPDATE VALID USERNAME/PASS***************"
+  res = update_controller(test_controller_id,username=test_username,password=test_password,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****CHECK OPERSTATUS UP Again***************"
+  print "*****USES VALUES SET in ADD COMMAND***************"
+#Added to make sure audit operations complete!!
+  time.sleep (15)
+  res = validate_controller_attributes(test_controller_id,version=test_version,ipaddr=test_ipaddr,operstatus ="up",auditstatus="enable",username=test_username,password=test_password,controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+
+  print "*****DELETE CONTROLLER***************"
+  res = delete_controller(controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+  print "*****VALIDATE DELETE***************"
+  res = is_controller_deleted(controller_url=test_url)
+  if res == 0:
+    print "Test Success"
+  else:
+    print "Test Failed!!!"
+    exit ()
+
+
+def add_controller_ex(blockname):
+  test_ipaddr=vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['ipaddr']
+  test_controller_id=vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['controller_id']
+  test_description = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['description']
+  test_type = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['type']
+  test_version = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['version']
+  test_auditstatus = vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['auditstatus']
+  test_username =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['username']
+  test_password =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['password']
+  test_invalusername =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['invalidusername']
+  test_invalpassword =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['invalidpassword']
+  test_url =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['url']
+  add_url=vtn_testconfig.ReadValues(CONTROLLERDATA ,'URL')['url']
+
+  res=add_controller(test_controller_id,test_type,test_version,controller_url=add_url,ipaddr=test_ipaddr,auditstatus=test_auditstatus,username=test_username,password=test_password)
+  return res
+
+
+def delete_controller_ex(blockname):
+  test_url =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['url']
+  return delete_controller(test_url)
+
+
+def update_controller_ex(blockname,ipaddr="",version="",auditstatus="",
+    description="",username="", password=""):
+  test_controller_id=vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['controller_id']
+  test_url =  vtn_testconfig.ReadValues(CONTROLLERDATA ,blockname)['url']
+  return update_controller(test_controller_id,ipaddr=ipaddr,version=version,
+      auditstatus=auditstatus,description=description,
+      username=username,password=password,controller_url=test_url)
+
+
+
+# Main Block
+if __name__ == '__main__':
+  print '*****CONTROLLER TESTS******'
+  test_controller_ops('ControllerFirst')
+else:
+  print "Controller Loaded as Module"
+  
---
+++ coordinator/test/vtn_ft/vtn_vbr.py
@@ -0,0 +1,476 @@
+#
+# Copyright (c) 2013 NEC Corporation
+# All rights reserved.
+# 
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this
+# distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+#
+
+
+#! /usr/bin/python
+
+import requests, json, collections, time, controller
+import vtn_testconfig
+
+VTNVBRDATA=vtn_testconfig.VTNVBRDATA
+CONTROLLERDATA=vtn_testconfig.CONTROLLERDATA
+
+coordinator_url=vtn_testconfig.coordinator_url
+def_header=vtn_testconfig.coordinator_headers
+controller_headers=vtn_testconfig.controller_headers
+controller_url_part=vtn_testconfig.controller_url_part
+
+
+
+def create_vtn(blockname):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,blockname)['vtn_name']
+  test_vtn_description=vtn_testconfig.ReadValues(VTNVBRDATA,blockname)['description']
+  vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,'VTNURL')['url']
+  url= coordinator_url + vtn_url 
+  print url
+
+  vtn_add = collections.defaultdict(dict)
+
+  vtn_add['vtn']['vtn_name']=test_vtn_name
+  vtn_add['vtn']['description']=test_vtn_description
+  r = requests.post(url,data=json.dumps(vtn_add),headers=def_header)
+  print r.status_code
+  if r.status_code != 200:
+    return 1
+  else:
+    return 0
+
+
+def delete_vtn(blockname):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,blockname)['vtn_name']
+  test_vtn_description=vtn_testconfig.ReadValues(VTNVBRDATA,blockname)['description']
+  vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,blockname)['vtn_url']
+  url= coordinator_url + vtn_url + '.json'
+  print url
+
+  r = requests.delete(url,headers=def_header)
+  print r.status_code
+  if r.status_code != 200:
+    return 1
+  else:
+    return 0
+
+
+
+def validate_vtn_at_controller(vtn_blockname, controller_blockname, presence="yes",position=0):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_name']
+  test_controller_ipaddr=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['ipaddr']
+  test_controller_port=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['port']
+  test_vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,'VTNURL')['ctr_url']
+
+  url='http://'+test_controller_ipaddr+':'+test_controller_port+controller_url_part+test_vtn_url
+  print url
+  r = requests.get(url,headers=controller_headers,auth=('admin','admin'))
+
+  print r.status_code
+
+  if presence == "no":
+    if r.status_code == 404:
+      return 0
+  if r.status_code != 200:
+    return 1
+
+  
+  data=json.loads(r.text)
+
+  print data
+
+  if presence == "no":
+    print data['vtn']
+    if data['vtn'] == []:
+      return 0
+
+  vtn_content=data['vtn'][position]
+
+  if vtn_content == None:
+    if presence == "yes":
+      return 0
+    else:
+      return 1
+
+  if vtn_content['name'] != test_vtn_name:
+    if presence == "yes":
+      return 1
+    else:
+      return 0
+  else:
+    if presence == "yes":
+      return 0
+    else:
+      return 1
+
+
+
+def create_vbr(vtn_blockname,vbr_blockname,controller_blockname):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_name']
+  test_vbr_name=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['vbr_name']
+  test_vbr_controller_id=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['controller_id']
+  test_vbr_description=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['description']
+  test_vbr_domain_id =vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['domain_id']
+  test_vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_url']
+  vbr_url=vtn_testconfig.ReadValues(VTNVBRDATA,'VBRURL')['url']
+
+
+  url= coordinator_url + test_vtn_url + vbr_url
+
+  print "url is ", url
+
+  vbr_add = collections.defaultdict(dict);
+  vbr_add['vbridge']['vbr_name']=test_vbr_name
+  vbr_add['vbridge']['controller_id']=test_vbr_controller_id
+  vbr_add['vbridge']['domain_id']=test_vbr_domain_id
+  vbr_add['vbridge']['description']=test_vbr_description
+
+
+  r = requests.post(url,data=json.dumps(vbr_add),headers=def_header)
+  print r.status_code
+  if r.status_code != 200:
+    return 1
+  else:
+    return 0
+
+
+
+def delete_vbr(vtn_blockname,vbr_blockname):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_name']
+  test_vbr_name=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['vbr_name']
+  test_vbr_description=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['description']
+  test_vbr_domain_id =vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['domain_id']
+  test_vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_url']
+  vbr_url=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['vbr_url']
+
+  url= coordinator_url + test_vtn_url + vbr_url + '.json'
+
+  print url
+
+  r = requests.delete(url,headers=def_header)
+  print r.status_code
+  if r.status_code != 200:
+    return 1
+  else:
+    return 0
+
+
+
+  
+def validate_vbr_at_controller(vtn_blockname, vbr_blockname,controller_blockname, presence="yes",position=0):
+  test_vtn_name=vtn_testconfig.ReadValues(VTNVBRDATA,vtn_blockname)['vtn_name']
+  test_vbr_name=vtn_testconfig.ReadValues(VTNVBRDATA,vbr_blockname)['vbr_name']
+  test_controller_ipaddr=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['ipaddr']
+  test_controller_id=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['controller_id']
+  test_controller_port=vtn_testconfig.ReadValues(CONTROLLERDATA,controller_blockname)['port']
+  test_vtn_url=vtn_testconfig.ReadValues(VTNVBRDATA,'VTNURL')['ctr_url']
+  test_vbr_url=vtn_testconfig.ReadValues(VTNVBRDATA,'VBRURL')['ctr_url']
+
+  url='http://'+test_controller_ipaddr+':'+test_controller_port+controller_url_part+test_vtn_url+'/'+test_vtn_name+test_vbr_url
+  print url
+  r = requests.get(url,headers=controller_headers,auth=('admin','admin'))
+  print r.status_code
+
+  if presence == "no":
+    if r.status_code == 404:
+      return 0
+
+  if r.status_code != 200:
+    return 1
+
+  
+  data=json.loads(r.text)
+
+  vtn_content=data['vbridge'][position]
+
+  print vtn_content
+
+  if vtn_content == None:
+    if presence == "yes":
+      return 0
+    else:
+      return 1
+
+
+  if vtn_content['name'] != test_vbr_name:
+    if presence == "yes":
+      return 1
+    else:
+      return 0
+  else:
+    if presence == "yes":
+      return 0
+    else:
+      return 1
+
+
+
+def test_vtn_vbr():
+
+  print "CREATE Controller"
+  retval = controller.add_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "Controller Create Failed"
+    exit(1)
+
+  print "TEST 1 : SIMPLE VTenant with one VBridge"
+ # Delay for AUDIT
+  time.sleep(15)
+  retval=create_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=create_vbr('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst')
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval = delete_vbr('VtnOne','VbrOne')
+  if retval != 0:
+    print "VBR/VTN Delete Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval = delete_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Delete Failed in coordinator"
+    exit(1)
+
+  print "DELETE CONTROLLER"
+  retval=controller.delete_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "CONTROLLER delete failed"
+    exit(1)
+  print "VTN->VBR TEST SUCCESS"
+
+
+def test_multi_vtn_with_vbr_test():
+
+  print "CREATE Controller"
+  retval = controller.add_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "Controller Create Failed"
+    exit(1)
+
+  time.sleep(15)
+  print "TEST 2 : 2 Tenants with one VBridge each"
+  print "VTNONE->VBRONE"
+  print "VTNTWO->VBRONE"
+
+  retval=create_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=create_vtn('VtnTwo')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=create_vbr('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=create_vbr('VtnTwo','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst',position=0)
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnTwo','ControllerFirst',position=1)
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnTwo','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+
+  retval = delete_vbr('VtnOne','VbrOne')
+  if retval != 0:
+    print "VBR/VTN Delete Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval = delete_vbr('VtnTwo','VbrOne')
+  if retval != 0:
+    print "VBR/VTN Delete Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnTwo','VbrOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnTwo','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval = delete_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Delete Failed in coordinator"
+    exit(1)
+
+
+  retval = delete_vtn('VtnTwo')
+  if retval != 0:
+    print "VTN Delete Failed in coordinator"
+    exit(1)
+
+  print "DELETE CONTROLLER"
+  retval=controller.delete_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "CONTROLLER delete failed"
+    exit(1)
+
+  print "MULTI VTN->VBR TEST SUCCESS"
+
+
+def test_vtn_multi_vbr_test():
+
+  print "CREATE Controller"
+  retval = controller.add_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "Controller Create Failed"
+    exit(1)
+
+  time.sleep(15)
+  print "TEST 3 : 1 Tenants with two VBridge"
+  print "VTNONE->VBRONE"
+  print "VTNONE->VBRTWO"
+
+  retval=create_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Create Failed"
+    exit(1)
+
+  retval=create_vbr('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VBR Create Failed"
+    exit(1)
+ 
+  retval=create_vbr('VtnOne','VbrTwo','ControllerFirst')
+  if retval != 0:
+    print "VBR Create Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst',position=0)
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VBROne Validate Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrTwo','ControllerFirst',position=1)
+  if retval != 0:
+    print "VBRTwo Validate Failed"
+    exit(1)
+
+  retval = delete_vbr('VtnOne','VbrTwo')
+  if retval != 0:
+    print "VBR/VTN Delete Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrTwo','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VtnVBRTwo Validate Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+  if retval != 0:
+    print "VtnVBR Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst')
+  if retval != 0:
+    print "VTN Validate Failed"
+    exit(1)
+
+  retval = delete_vbr('VtnOne','VbrOne')
+  if retval != 0:
+    print "VBR/VTN Delete Failed"
+    exit(1)
+
+  retval=validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+  if retval != 0:
+    print "VBR Validate Failed"
+    exit(1)
+
+  retval=validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+  if retval != 0:
+     print "VTN Validate Failed"
+     exit(1)
+
+  retval = delete_vtn('VtnOne')
+  if retval != 0:
+    print "VTN Delete Failed in coordinator"
+    exit(1)
+
+
+  print "DELETE CONTROLLER"
+  retval=controller.delete_controller_ex('ControllerFirst')
+  if retval != 0:
+    print "CONTROLLER delete failed"
+    exit(1)
+
+  print "MULTI VBR IN ONE VTN->VBR TEST SUCCESS"
+
+# Main Block
+if __name__ == '__main__':
+  print '*****CONTROLLER TESTS******'
+  test_vtn_vbr()
+  test_multi_vtn_with_vbr_test()
+  test_vtn_multi_vbr_test()
+
+else:
+  print "VTN VBR Loaded as Module"
+
