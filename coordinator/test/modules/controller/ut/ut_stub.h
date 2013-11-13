93fe7a4d152a7b446a25e374a3d41f66d4e1cf37
kamatchi <kamatchi-l@hcl.com>
2013-11-13 16:09:39 +0530

    Tests for Controller handling in vtndrvintf module in ODC driver daemon
    Change-Id: I2e57a579d06ba49ef2cd08029658beaedf0784f1
    Signed-off-by: kamatchi <kamatchi-l@hcl.com>

---
+++ coordinator/test/modules/controller/ut/ut_stub.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013 NEC Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#ifndef _TEST_CONTROLLER_UT_STUB_H
+#define _TEST_CONTROLLER_UT_STUB_H
+
+/*
+ * Include stub header files.
+ */
+
+#include "stub/include/core_include/pfc/ipc_client.h"
+
+#ifdef __cplusplus
+#include "stub/include/cxx/pfcxx/ipc_server.hh"
+#include "stub/include/cxx/pfcxx/ipc_client.hh"
+#include "stub/include/cxx/pfcxx/module.hh"
+#include "stub/include/cxx/pfcxx/timer.hh"
+#include "stub/include/cxx/pfcxx/task_queue.hh"
+#include "stub/tclib_module/tclib_module.hh"
+#include "stub/tclib_module/tclib_struct_defs.hh"
+#include "stub/odcdriver/odc_controller.hh"
+#include "stub/odcdriver/odc_drv.hh"
+#include "stub/vtncacheutil/include/keytree.hh"
+#include "stub/vtncacheutil/include/confignode.hh"
+#include "stub/vtncacheutil/include/vtn_conf_data_element_op.hh"
+#include "stub/vtncacheutil/include/vtn_conf_utility.hh"
+#endif /* __cplusplus */
+
+#endif /* !_TEST_CONTROLLER_UT_STUB_H */
