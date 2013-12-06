7d836816fc1ee6fe475525e1516ca2d0a8268dac
balakrishnan <balakrishnan.ka@hcl.com>
2013-12-06 09:56:34 +0530

    Functional test for Audit scenarios with vtn,vbr and portmap configuration entries
    Change-Id: Ia96b82e4f18decee6e3191b9abb7b04eeb721c23
    Signed-off-by: balakrishnan <balakrishnan.ka@hcl.com>

--- coordinator/test/vtn_ft/vtn_vbr.py
+++ coordinator/test/vtn_ft/vtn_vbr.py
@@ -464,13 +464,304 @@ def test_vtn_multi_vbr_test():
 
     print "MULTI VBR IN ONE VTN->VBR TEST SUCCESS"
 
+def test_vtn_vbr_audit_1():
+
+    print "****CREATE Controller with valid IP****"
+    retval = controller.add_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "Controller Create Failed"
+        exit(1)
+
+    print """TEST 4 : create VTN and VBR when controller is up
+             change the controller status to down delete VTN and VBR
+             trigger Audit"""
+  # Delay for AUDIT
+    time.sleep(15)
+    print "****Create VTN****"
+    retval = create_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    print "****Create VBR****"
+    retval = create_vbr('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    print "****UPDATE Controller IP to invalid****"
+    test_invalid_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['invalid_ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_invalid_ipaddr)
+    if retval != 0:
+        print "controller invalid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    print "****Delete VBR****"
+    retval = delete_vbr('VtnOne','VbrOne')
+    if retval != 0:
+        print "VBR/VTN Delete Failed"
+        exit(1)
+
+    print "****Delete VTN****"
+    retval = delete_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Delete Failed in coordinator"
+        exit(1)
+
+    print "****UPDATE Controller IP to Valid****"
+    test_controller_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_controller_ipaddr)
+    if retval != 0:
+        print "controller valid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+
+    print "DELETE CONTROLLER"
+    retval = controller.delete_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "CONTROLLER delete failed"
+        exit(1)
+    print "VTN->VBR AUDIT TEST SUCCESS"
+
+def test_vtn_vbr_audit_2():
+
+    print "****CREATE Controller with valid IP****"
+    retval = controller.add_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "Controller Create Failed"
+        exit(1)
+
+    print "****UPDATE Controller IP to invalid****"
+    test_invalid_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['invalid_ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_invalid_ipaddr)
+    if retval != 0:
+        print "controller invalid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    print """TEST 5 : create VTN and VBR when controller is down
+             change the controller status to up trigger Audit"""
+    print "****Create VTN****"
+    retval = create_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    print "****Create VBR****"
+    retval = create_vbr('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    print "****UPDATE Controller IP to Valid****"
+    test_controller_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_controller_ipaddr)
+    if retval != 0:
+        print "controller valid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+
+    print "****Delete VBR****"
+    retval = delete_vbr('VtnOne','VbrOne')
+    if retval != 0:
+        print "VBR/VTN Delete Failed"
+        exit(1)
+
+    print "****Delete VTN****"
+    retval = delete_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Delete Failed in coordinator"
+        exit(1)
+
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+
+    print "DELETE CONTROLLER"
+    retval = controller.delete_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "CONTROLLER delete failed"
+        exit(1)
+    print "VTN->VBR AUDIT TEST SUCCESS"
+
+def test_multi_vtn_with_vbr_audit_test():
+
+    print "CREATE Controller"
+    retval = controller.add_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "Controller Create Failed"
+        exit(1)
+
+    time.sleep(15)
+    print "TEST 6 : 2 Tenants with one VBridge each and test AUDIT"
+    print "VTNONE->VBRONE"
+    print "VTNTWO->VBRONE"
+
+    retval = create_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    retval = create_vtn('VtnTwo')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    retval = create_vbr('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    retval = create_vbr('VtnTwo','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VTN Create Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst',position=0)
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnTwo','ControllerFirst',position=1)
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    retval =  validate_vbr_at_controller('VtnTwo','VbrOne','ControllerFirst')
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    print "****UPDATE Controller IP to invalid****"
+    test_invalid_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['invalid_ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_invalid_ipaddr)
+    if retval != 0:
+        print "controller invalid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    retval = delete_vbr('VtnOne','VbrOne')
+    if retval != 0:
+        print "VBR/VTN Delete Failed"
+        exit(1)
+
+    retval = delete_vbr('VtnTwo','VbrOne')
+    if retval != 0:
+        print "VBR/VTN Delete Failed"
+        exit(1)
+
+    print "****UPDATE Controller IP to Valid****"
+    test_controller_ipaddr= vtn_testconfig.ReadValues(CONTROLLERDATA,'ControllerFirst')['ipaddr']
+    retval = controller.update_controller_ex('ControllerFirst',ipaddr=test_controller_ipaddr)
+    if retval != 0:
+        print "controller valid_ip update failed"
+        exit(1)
+  # Delay for AUDIT
+    time.sleep(45)
+
+    retval =  validate_vbr_at_controller('VtnOne','VbrOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+
+    retval =  validate_vbr_at_controller('VtnTwo','VbrOne','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VBR Validate Failed"
+        exit(1)
+
+    retval =  validate_vtn_at_controller('VtnTwo','ControllerFirst',presence="no")
+    if retval != 0:
+        print "VTN Validate Failed"
+        exit(1)
+
+    retval = delete_vtn('VtnOne')
+    if retval != 0:
+        print "VTN Delete Failed in coordinator"
+        exit(1)
+
+
+    retval = delete_vtn('VtnTwo')
+    if retval != 0:
+        print "VTN Delete Failed in coordinator"
+        exit(1)
+
+    print "DELETE CONTROLLER"
+    retval = controller.delete_controller_ex('ControllerFirst')
+    if retval != 0:
+        print "CONTROLLER delete failed"
+        exit(1)
+
+    print "MULTI VTN->VBR AUDIT TEST SUCCESS"
+
 # Main Block
 if __name__ == '__main__':
     print '*****CONTROLLER TESTS******'
     test_vtn_vbr()
     test_multi_vtn_with_vbr_test()
     test_vtn_multi_vbr_test()
-
+    test_vtn_vbr_audit_1()
+    test_vtn_vbr_audit_2()
+    test_multi_vtn_with_vbr_audit_test()
 else:
     print "VTN VBR Loaded as Module"
 
