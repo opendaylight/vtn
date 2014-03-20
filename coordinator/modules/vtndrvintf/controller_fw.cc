c9eaf5ff10b709876adf934689dee4ec2f1dc145
kamatchi <kamatchi-l@hcl.com>
2014-03-20 11:37:35 +0530

    Fix for bug 673.
    Change-Id: I5221f23dae6e1c111dc388616340e225640d8356
    Signed-off-by: kamatchi <kamatchi-l@hcl.com>

--- coordinator/modules/vtndrvintf/controller_fw.cc
+++ coordinator/modules/vtndrvintf/controller_fw.cc
@@ -262,7 +262,9 @@ void ReadConfig::GetPhysicalConfig() {
   }
   if ((ctr_instance->get_connection_status() == CONNECTION_DOWN) ||
       (ctr_instance->audit_result_ == PFC_FALSE)) {
-    pfc_log_error("Audit failed in controller");
+    pfc_log_error("Controller down/Audit failed in controller");
+    ctr_fw_->post_physical_taskq(ctlr_name_, drv_instance, ctr_instance,
+                                  ctr_fw_->time_interval_);
     return;
   }
   pfc_bool_t ping_status =
