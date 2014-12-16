063eb35f2742cf8d953d60ff5af065ca59052a4c
kavitha.ma <kavitha.ma@hcl.com>
2014-12-16 22:44:06 +0530

    VTN Client App for getting the running configuration of VTN.
    Change-Id: I0ca2815f1d3f53ed4d507aec5dfc9e973b4bef12
    Signed-off-by: kavitha.ma <kavitha.ma@hcl.com>

---
+++ application/list_vtn_configuration/src/main/java/org/opendaylight/vtn/app/run/config/json/annotations/JsonElement.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2014 NEC Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.vtn.app.run.config.json.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * JsonElement Interface provides functions to for name and defaultvalue to set
+ *    in the implementation.
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface JsonElement {
+
+    /**
+     * name - provides api to set the name in the implementation.
+     * @return
+     */
+    String name();
+
+    /**
+     * defaultValue - provide api to set the defaultValue in the implementation.
+     * @return
+     */
+    String defaultValue() default "";
+}
