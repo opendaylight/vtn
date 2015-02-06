59ca339cfa044fa2fd48ea306b555f57ce34422b
Shigeru Yasuda <s-yasuda@da.jp.nec.com>
2015-02-07 00:17:35 +0900

    Fixed build error due to ovsdb change.
    Change-Id: I49e95a3f1db923e7710ba4827af61d079eec2b67
    Signed-off-by: Shigeru Yasuda <s-yasuda@da.jp.nec.com>

--- manager/neutron/src/test/java/org/opendaylight/vtn/manager/neutron/ConnectionServiceInternalStub.java
+++ manager/neutron/src/test/java/org/opendaylight/vtn/manager/neutron/ConnectionServiceInternalStub.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 NEC Corporation
+ * Copyright (c) 2014-2015 NEC Corporation
  * All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -13,9 +13,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.opendaylight.ovsdb.plugin.api.Connection;
+import org.opendaylight.ovsdb.plugin.api.ConnectionConstants;
 import org.opendaylight.ovsdb.plugin.api.OvsdbConnectionService;
+
 import org.opendaylight.controller.sal.core.Node;
-import org.opendaylight.controller.sal.connection.ConnectionConstants;
 
 /**
  * Stub class for unit tests.
