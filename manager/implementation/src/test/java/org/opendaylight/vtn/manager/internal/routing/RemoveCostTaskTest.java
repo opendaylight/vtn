877efe5f4c0ee0fec97c1100d3572f52dceb659c
dhandapani <dhandapani.l@hcl.com>
2015-04-09 11:26:25 +0530

    Add Unit Test for routing and internal package.
    Change-Id: Ic27e87c9fa3c3673bf0774f9182f601845b36120
    Signed-off-by: dhandapani <dhandapani.l@hcl.com>

---
+++ manager/implementation/src/test/java/org/opendaylight/vtn/manager/internal/routing/RemoveCostTaskTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 NEC Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.vtn.manager.internal.routing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.types.rev150209.VtnPortDesc;
+
+/**
+  * Unit Test for RemoveCostTask.
+  */
+public class RemoveCostTaskTest extends Assert {
+
+    /**
+     * Set the Port ID.
+     */
+    private static final int ID = 20;
+
+    /**
+     * Return the target switch port descriptor.
+     *
+     * Test Case for VtnPortDesc.
+     */
+    @Test
+    public void testGetPortDesc() {
+        VtnPortDesc[] portDescs = {
+            null,
+            new VtnPortDesc("openflow:1,,"),
+            new VtnPortDesc("openflow:1,2,"),
+            new VtnPortDesc("openflow:1,2,eth2")
+        };
+        for (VtnPortDesc port : portDescs) {
+            RemoveCostTask removeCostTask = new RemoveCostTask(ID, port);
+            assertEquals(port, removeCostTask.getPortDesc());
+        }
+    }
+}
