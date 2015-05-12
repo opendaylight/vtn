799a2a8a41c14d051c281f50604b92737f11b260
bharathi.m <bharathi.m@hcl.com>
2015-05-12 13:13:32 +0530

    Support for Microsoft SCVMM
       * Add VTN Coordinator as Network Service
       * GUI support for configuration REFRESH
       * Coordinator uses Opendaylight installed with VTN 
         for any network requests.
       * Https support for VTN Coordinator.
    
    Change-Id: I4af8d3421e61947bc921f915c752e18bbfe6035c
    Signed-off-by: bharathi.m <bharathi.m@hcl.com>

---
+++ application/ODL_VSEM_PROVIDER_SRC/VSEMOdlProvider/Cmdlets/GetVSEMLogicalNetwork.cs
@@ -0,0 +1,152 @@
+﻿//     Copyright (c) 2015 NEC Corporation
+//     All rights reserved.
+//
+//     This program and the accompanying materials are made available under the
+//     terms of the Eclipse Public License v1.0 which accompanies this
+//     distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Management.Automation;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Web.Script.Serialization;
+using Microsoft.SystemCenter.NetworkService;
+using ODL.VSEMProvider.Cmdlets.Common;
+using ODL.VSEMProvider.Libraries;
+using ODL.VSEMProvider.Libraries.Common;
+using ODL.VSEMProvider.VSEMEvents;
+using VSEM.Cmdlets.Common;
+
+namespace ODL.VSEMProvider.Cmdlets {
+    /// <summary>
+    /// This class represents a cmdlet. This cmdlet is used to retrieves the information about the
+    /// fabric network definition(s) that are managed by the virtual switch extension manager.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "Odl.VSEMLogicalNetwork")]
+    [OutputType(typeof(List<LogicalNetwork>))]
+    [ManagedName("Microsoft.SystemCenter.NetworkService.GetLogicalNetwork")]
+    public sealed class GetVSEMLogicalNetwork : VSEMODLCmdletBase {
+        /// <summary>
+        /// Logical Network Definition Id.
+        /// </summary>
+        private Guid iD;
+
+        /// <summary>
+        /// Fabric Network Definition Id.
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public Guid ID {
+            get {
+                return this.iD;
+            }
+
+            set {
+                this.iD = value;
+            }
+        }
+
+        /// <summary>
+        /// Connection object returned by the Open Connection cmdlet with the target extension
+        /// manager. This is a mandatory parameter.
+        /// </summary>
+        private IConnection connection;
+
+        /// <summary>
+        /// This parameter specifies Connection object.
+        /// </summary>
+        [Parameter(ValueFromPipeline = true)]
+        public IConnection Connection {
+            get {
+                return this.connection;
+            }
+
+            set {
+                this.connection = value;
+            }
+        }
+
+        /// <summary>
+        /// The connection object parsed to VSEMConnection.
+        /// </summary>
+        private VSEMConnection conn;
+
+        /// <summary>
+        /// This function is responsible for validating the parameters.
+        /// </summary>
+        protected override void BeginODLVSEMCmdlet() {
+            this.conn = VSEMODLCmdletUtility.ValidateConnectionObject(this.Connection, this.CmdletName);
+        }
+
+        /// <summary>
+        /// This function is responsible to retrieve the VSEMFabricNetworkDefinition information
+        /// for the connection.
+        /// </summary>
+        protected override void DoODLVSEMCmdlet() {
+            var JavaScriptSerializer = new JavaScriptSerializer();
+            JavaScriptSerializer.MaxJsonLength = int.MaxValue;
+            StringBuilder json = new StringBuilder(" \"ID\":" + JavaScriptSerializer.Serialize(this.ID));
+            ODLVSEMETW.EventWriteDoCmdlet(this.CmdletName,
+                "Retrieving logical network definition.",
+                json.ToString());
+            List<LogicalNetwork> vmNw = null;
+            TransactionManager txnMng = new TransactionManager();
+            txnMng.StartTransaction();
+            var operation = TransactionManager.Operation.None;
+            try {
+                if (this.ID != Guid.Empty) {
+                    vmNw = new List<LogicalNetwork>();
+                    vmNw.Add(this.conn.GetVSEMLogicalNetworkById(txnMng, this.ID));
+                    ODLVSEMETW.EventWriteReturnLibrary(string.Format(CultureInfo.CurrentCulture,
+                        "Logical network definition is retrieved by ID: {0}.",
+                        this.ID.ToString("B")),
+                        string.Empty);
+
+                    if (vmNw[0] == null) {
+                        ODLVSEMETW.EventWriteProcessCmdletWarning(this.CmdletName,
+                        string.Format(CultureInfo.CurrentCulture,
+                        "ID '{0}' not found.",
+                        this.ID.ToString("B")));
+                        this.WriteWarning(string.Format(CultureInfo.CurrentCulture,
+                            "ID '{0}' not found.",
+                            this.ID.ToString("B")));
+                    } else {
+                        var hnvmgmt = new HNVLogicalNetworkManagement(this.conn.GetVtnHostName());
+
+                        // Validate HNV LogicalNetwork. Need not check return value.
+                        hnvmgmt.IsLogicalNetworkValid(vmNw[0]);
+                        this.WriteObject(vmNw, true);
+                    }
+                } else {
+                    vmNw = this.conn.GetVSEMLogicalNetwork(txnMng);
+                    ODLVSEMETW.EventWriteReturnLibrary(
+                        "Logical networks are retrieved.", string.Empty);
+                    if (vmNw == null) {
+                        ODLVSEMETW.EventWriteProcessCmdletWarning(this.CmdletName,
+                            "Logical network not found.");
+                        this.WriteWarning("Logical network not found.");
+                    } else {
+                        var hnvmgmt = new HNVLogicalNetworkManagement(this.conn.GetVtnHostName());
+                        for (int i = 0; i < vmNw.Count; i++) {
+                            // Validate HNV LogicalNetworks. Need not check return value.
+                            hnvmgmt.IsLogicalNetworkValid(vmNw[i]);
+                        }
+                        this.WriteObject(vmNw, true);
+                    }
+                }
+            } catch (Exception ex) {
+                Exception exception = VSEMODLExceptionUtil.ConvertExceptionToVSEMException(ex);
+                ODLVSEMETW.EventWriteFailedCmdlet(this.CmdletName, exception.GetType() + " : " + ex.Message);
+                operation = TransactionManager.Operation.Rollback;
+                throw exception;
+            } finally {
+                txnMng.EndTransaction(operation);
+
+                string output = "\"LogicalNetwork\":" + JavaScriptSerializer.Serialize(vmNw);
+                ODLVSEMETW.EventWriteEndCmdlet(this.CmdletName, output);
+            }
+        }
+    }
+}
---
+++ application/ODL_VSEM_PROVIDER_SRC/VSEMOdlProvider/Libraries/Common/CustomEqualityRangeComparer.cs
@@ -0,0 +1,39 @@
+﻿//     Copyright (c) 2015 NEC Corporation
+//     All rights reserved.
+//
+//     This program and the accompanying materials are made available under the
+//     terms of the Eclipse Public License v1.0 which accompanies this
+//     distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+
+using System.Collections.Generic;
+
+namespace ODL.VSEMProvider.Libraries.Common {
+    /// <summary>
+    /// Custom equality compararer for range.
+    /// </summary>
+    public class CustomEqualityRangeComparer : IEqualityComparer<Range> {
+        /// <summary>
+        /// Overrides Equals method of IEqualityComparer.
+        /// </summary>
+        /// <param name="x">The first object of type Range to compare.</param>
+        /// <param name="y">The second object of type Range to compare.</param>
+        /// <returns>true if both object are equal.</returns>
+        public bool Equals(Range x, Range y) {
+            if (x.Start.CompareTo(y.Start) == 0 && x.End.CompareTo(y.End) == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Overrides GetHashCode method of IEqualityComparer.
+        /// </summary>
+        /// <param name="bx">Range object.</param>
+        /// <returns>A hash code for the specified object.</returns>
+        public int GetHashCode(Range bx) {
+            int code = bx.Start ^ bx.End;
+            return code.GetHashCode();
+        }
+    }
+}
