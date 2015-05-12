c3f7df4e90d2ac7d2de95aa3a90722f82a37fee2
bharathi.m <bharathi.m@hcl.com>
2015-05-12 13:13:32 +0530

    Support for Microsoft SCVMM
       * Add VTN as Network Service
       * GUI support for configuration REFRESH
    
    Change-Id: I4af8d3421e61947bc921f915c752e18bbfe6035c
    Signed-off-by: bharathi.m <bharathi.m@hcl.com>

---
+++ application/ODL_VSEM_PROVIDER_SRC/VSEMOdlProvider/Libraries/Entity/VSEMInfo.cs
@@ -0,0 +1,41 @@
+﻿//     Copyright (c) 2015 NEC Corporation
+//     All rights reserved.
+//
+//     This program and the accompanying materials are made available under the
+//     terms of the Eclipse Public License v1.0 which accompanies this
+//     distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ODL.VSEMProvider.Libraries.Entity {
+    /// <summary>
+    /// This class describes the information about the ODL which is managed by Virtual Switch Extension Manager.
+    /// </summary>
+    [Serializable, DataContract]
+    public class VSEMInfo {
+        /// <summary>
+        /// VTNCoordinatoor Host name.
+        /// </summary>
+        [DataMember]
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// ODL port no.
+        /// </summary>
+        [DataMember]
+        public long Port { get; set; }
+
+        /// <summary>
+        /// Minimum version of VTNCoordinator allowed for connection with VSEMProvider.
+        /// </summary>
+        [DataMember]
+        public Version MinVTNCoVersion { get; set; }
+
+        /// <summary>
+        /// Time and Date of the last modification.
+        /// </summary>
+        [DataMember]
+        public DateTime LastModifiedTimeStamp { get; set; }
+    }
+}
