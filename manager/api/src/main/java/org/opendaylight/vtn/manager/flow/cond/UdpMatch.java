0ec153def31f2aa8fb8666066c0ddc8aaa37468e
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 12:21:14 -0400

    Fix license header violations in manager/api flow/cond
    Change-Id: Id4e71e21809aa208116e88b3c17ca766a91b72ea
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- manager/api/src/main/java/org/opendaylight/vtn/manager/flow/cond/UdpMatch.java
+++ manager/api/src/main/java/org/opendaylight/vtn/manager/flow/cond/UdpMatch.java
@@ -1,10 +1,9 @@
 /*
- * Copyright (c) 2014 NEC Corporation
- * All rights reserved.
+ * Copyright (c) 2014, 2015 NEC Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
 package org.opendaylight.vtn.manager.flow.cond;
