83049af9fa7621b2dc874b597d1f6289cc317f50
gvrangan <venkatrangang@hcl.com>
2016-01-22 17:15:33 -0500

    Bug 4772 Fix to add ethernet interface as port to the bridge.
    addPortToBridge method was adding the termination point
    to the parent node instead of the bridge node entry.
    Current fix fetches the bridge node from the configuration
    DS  after bridge is added and then adds the termination point.
    
    Change-Id: Id7a7eadf2b7be2d15495d7475e6bdfe214934420
    Signed-off-by: gvrangan <venkatrangang@hcl.com>
    (cherry picked from commit 5ff2710011995ea040a4a9d4a85b6613a56320f8)

--- manager/neutron/src/test/java/org/opendaylight/vtn/manager/neutron/impl/OVSDBEventHandlerTest.java
+++ manager/neutron/src/test/java/org/opendaylight/vtn/manager/neutron/impl/OVSDBEventHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 NEC Corporation. All rights reserved.
+ * Copyright (c) 2015, 2016 NEC Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -179,8 +179,13 @@ public final class OVSDBEventHandlerTest {
     @Test
     public void testAddPortToBridge() throws Exception {
         Node mockNode = PowerMockito.mock(Node.class);
+        InstanceIdentifier<Node> mockInstanceIdentifier  =
+                                   PowerMockito.mock(InstanceIdentifier.class);
         Mockito.when(mockNode.getNodeId()).
                 thenReturn(Mockito.mock(NodeId.class));
+        PowerMockito.doReturn(mockNode).
+                       when(handler , "getBridgeConfigNode" ,
+                            mockNode , "bridge1");
         PowerMockito.
                   doReturn(Mockito.
                            mock(OvsdbTerminationPointAugmentation.class)).
